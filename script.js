3c37366c78cccff478cbc0b2dd87ddebee2084ca
jimmimmim <likecharry@gmail.com>
2022-01-20 17:41:40 +0900

    Attach header & footer

--- script.js
+++ script.js
@@ -15,3 +15,20 @@ window.addEventListener('load', function () {
         }
     });
 });
+
+// <!-- FAQ관련 스크립트-->
+const items = document.querySelectorAll('.question');
+
+function openCloseAnswer() {
+    const answerId = this.id.replace('que', 'ans');
+
+    if (document.getElementById(answerId).style.display === 'block') {
+        document.getElementById(answerId).style.display = 'none';
+        document.getElementById(this.id + '-toggle').textContent = '∨';
+    } else {
+        document.getElementById(answerId).style.display = 'block';
+        document.getElementById(this.id + '-toggle').textContent = '∧';
+        }
+}
+
+items.forEach(item => item.addEventListener('click', openCloseAnswer));
